3396b3ecad9bc5289d1115e18aca612983923de2
Obay Mardini <obaymardini@gmail.com>
2021-11-29 21:53:25 +0100

    test

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ app.get("/favicon.ico", (req, res) => {
   res.sendFile(path.resolve("favicon.ico"));
 });
 
+app.get("/", (req, res) => {
+  const htmlBody = template("index");
+  res
+    .type("text/html")
+    .status(200)
+    // trust google maps iFrame
+    .header("Content-Security-Policy", "script-src 'self' https://cdn.websitepolicies.io https://google.com")
+    .send(htmlBody);
+});
 
 app.get("/:pagename.html", (req, res) => {
   const htmlBody = template(req.params.pagename);
@@ -30,13 +39,7 @@ app.get("/:pagename.html", (req, res) => {
     .send(htmlBody);
 });
 
-app.get("/", (req, res) => {
-  const htmlBody = template("index");
-  res
-    .type("text/html")
-    .status(200)
-    .send(htmlBody);
-});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
