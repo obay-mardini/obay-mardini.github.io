197ae8a991496ca5a7da4348676d06ebd09fbd3f
Obay Mardini <obaymardini@gmail.com>
2023-01-29 15:25:06 +0100

    parcel

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require("express");
-const path = require("path");
-const helmet = require("helmet");
-const template = require("./template");
+// const path = require("path");
+// const helmet = require("helmet");
+// const template = require("./template");
 
 const app = express();
 
-app.use(helmet());
+// app.use(helmet());
 
-app.use("/css", express.static("html/css"));
-app.use("/favicons", express.static("html/favicons"));
-app.use("/fonts", express.static("html/fonts"));
-app.use("/img", express.static("html/img"));
-app.use("/js", express.static("html/js"));
-app.use("/styles", express.static("html/styles"));
-app.use("/vendor", express.static("html/vendor"));
+// app.use("/css", express.static("html/css"));
+// app.use("/favicons", express.static("html/favicons"));
+// app.use("/fonts", express.static("html/fonts"));
+// app.use("/img", express.static("html/img"));
+// app.use("/js", express.static("html/js"));
+// app.use("/styles", express.static("html/styles"));
+// app.use("/vendor", express.static("html/vendor"));
 
-app.get("/favicon.ico", (req, res) => {
-  res.sendFile(path.resolve("favicon.ico"));
-});
+// app.get("/favicon.ico", (req, res) => {
+//   res.sendFile(path.resolve("favicon.ico"));
+// });
 
 app.get("/", (req, res) => {
-  const htmlBody = template("index");
+  // const htmlBody = template("index");
   res
     .type("text/html")
     .status(200)
@@ -29,15 +29,15 @@ app.get("/", (req, res) => {
     .send(htmlBody);
 });
 
-app.get("/:pagename.html", (req, res) => {
-  const htmlBody = template(req.params.pagename);
-  res
-    .type("text/html")
-    .status(200)
-    // trust google maps iFrame
-    .header("Content-Security-Policy", "script-src 'self' https://cdn.websitepolicies.io https://google.com")
-    .send(htmlBody);
-});
+// app.get("/:pagename.html", (req, res) => {
+//   const htmlBody = template(req.params.pagename);
+//   res
+//     .type("text/html")
+//     .status(200)
+//     // trust google maps iFrame
+//     .header("Content-Security-Policy", "script-src 'self' https://cdn.websitepolicies.io https://google.com")
+//     .send(htmlBody);
+// });
 
 
 
--- template.js
+++ template.js
@@ -1,21 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-String.prototype.replaceAll = function(search, replacement) {
-  const target = this;
-  return target.replace(new RegExp(search, 'g'), replacement);
-};
-
-const include = (html, part) => {
-  return html.replaceAll(`__INCLUDE_${part.toUpperCase()}__`, fs.readFileSync(path.resolve(`html/partials/${part}.html`), 'utf8'));
-}
-
 module.exports = (page) => {
   let html = fs.readFileSync(path.resolve(`html/${page}.html`), 'utf8');
-  html = include(html, 'head');
-  html = include(html, 'sidebar');
-  html = include(html, 'content_head');
-  html = include(html, 'js');
-  html = include(html, 'preloader');
+  // html = include(html, 'head');
+  // html = include(html, 'sidebar');
+  // html = include(html, 'content_head');
+  // html = include(html, 'js');
+  // html = include(html, 'preloader');
   return html;
 };
